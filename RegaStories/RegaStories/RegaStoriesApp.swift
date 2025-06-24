887f92761db4bd766b5ff358a62e3ea97a47e457
Raul Regadera <raulregadera@gmail.com>
2025-06-24 17:22:31 +0200

    Initial setup

---
+++ RegaStories/RegaStories/RegaStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegaStoriesApp.swift
+//  RegaStories
+//
+//  Created by Raul Regadera on 24/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RegaStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
