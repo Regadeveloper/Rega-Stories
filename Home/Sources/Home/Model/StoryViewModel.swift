835b2abba63e179150f810acd68971f183c0687e
Raul Regadera <raulregadera@gmail.com>
2025-06-24 21:05:06 +0200

    Make placeholder story view

---
+++ Home/Sources/Home/Model/StoryViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  StoryViewModel.swift
+//  Home
+//
+//  Created by Raul Regadera on 24/6/25.
+//
+
--- Home/Sources/Home/View/StoryView.swift
+++ Home/Sources/Home/View/StoryView.swift
@@ -1,7 +1,39 @@
-//
-//  StoryView.swift
-//  Home
-//
-//  Created by Raul Regadera on 24/6/25.
-//
+import SwiftUI
 
+struct StoryView: View {
+    let imageURL: URL?
+
+    public init(imageURL: URL? = nil) {
+        self.imageURL = imageURL
+    }
+
+    var body: some View {
+        VStack {
+            AsyncImage(
+                url: imageURL,
+                content: { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                },
+                placeholder: {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.gray)
+                        .padding(20)
+                }
+            )
+            .background(Color.white)
+            .clipShape(Circle())
+            .frame(width: 80, height: 80)
+            .padding(.horizontal, 3)
+        }
+        Image(systemName: "heart.fill")
+            .foregroundColor(.red)
+            .padding(5)
+            .background(Color.white.opacity(0.8))
+            .clipShape(Circle())
+            .offset(x: 30, y: -30)
+    }
+}
